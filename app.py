65765be6f047c6861f63aa55c0fa5f0ad72846d0
Vishal Patel <vishalpatel@Vishals-MacBook-Pro.local>
2020-11-16 16:23:26 -0800

    data page reads s3 data

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, url_for, render_template, redirect, jsonify
 import pandas as pd
 
+
 app = Flask(__name__)
 
 
@@ -27,6 +28,12 @@ def overview():
 @app.route("/data")
 def data():
     return render_template("data.html")
+
+@app.route("/NBAData")
+def NBAData():
+    df = pd.read_csv("https://uci-dataproject3.s3-us-west-1.amazonaws.com/AllTimeNbaSeason4Categories.csv")
+    json_data = df.to_json(orient="records")
+    return json_data
 
 if __name__ == "__main__":
     app.run(debug=True)
